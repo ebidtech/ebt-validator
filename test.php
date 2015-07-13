030e0dd4a9fabb5ab545ffe50b5a7e998cb3d0a3
Diogo Teixeira <diogo.teixeira@emailbidding.com>
2015-07-13 15:25:52 +0100

    Delete test.php

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-
-$validator = new EBT\Validator\Service\Validator\ValidatorService();
-var_dump($validator->requiredPositiveInteger(1, 'test', 'param'));
-var_dump($validator->requiredPositiveInteger(2, 'test', 'param'));
-var_dump($validator->requiredPositiveInteger(0, 'test', 'param'));
-var_dump($validator->requiredPositiveInteger('asd', 'test', 'param'));
-var_dump($validator->requiredPositiveInteger(0, 'test', 'param', 'Exception'));
